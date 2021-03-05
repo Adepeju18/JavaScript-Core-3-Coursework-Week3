bf63ba405cf5aca5b74e4ca2befd490b47c41fe5
Adepeju <pejulawson18@gmail.com>
2021-03-05 13:48:22 +0000

    complete

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,4 +6,25 @@
      { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
      { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
  ]
- 
+//  const{itemName, quantity, unitPrice}=order;
+const[hotCakes, applePie, EggMcMuffin, SausageMcMuffin, hotCoffee, HashBrown]= order;
+ function priceReceipts(item){
+let total =0;
+
+item.forEach((element)=>{
+    const price = element.unitPrice * element.quantity;
+    total+=price;
+
+    // const temp = { 
+    //     QTY: element.quantity,
+    //     ITEM: element.itemName,
+    //     TOTAL: price.toFixed(2),
+    // };
+    // result.push(temp);
+});
+// console.table(order,["QTY,    ITEM,    TOTAL"]);
+    //  console.table([hotCakes, applePie, EggMcMuffin, SausageMcMuffin, hotCoffee, HashBrown], ["itemName"]);
+     console.table(order)
+console.log(`TOTAL: ${total}`);
+ }
+ priceReceipts(order);
